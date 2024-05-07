789ac2840f89c06163811664410f7e3df3feba28
deepfriedslipper <108072844+deepfriedslipper@users.noreply.github.com>
2024-05-07 08:51:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const slides = document.querySelectorAll('.slide');
 
+var count = 1;
+slides.forEach((slide) => {
+  slide.addEventListener('click', function () {
+    slide.classList.add('d-none');
+    document.getElementById('slide-' + count).classList.remove('d-none');
+  });
+  count++;
+});
