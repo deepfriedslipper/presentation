682ab673ca4cbeed68e9ca279a8ffdc45b02e73c
deepfriedslipper <108072844+deepfriedslipper@users.noreply.github.com>
2024-05-08 08:41:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const slides = document.querySelectorAll('.slide');
-
 var count = 2;
-slides.forEach((slide) => {
+document.querySelectorAll('.slide').forEach((slide) => {
   slide.addEventListener('click', function () {
     slide.classList.add('d-none');
     document.getElementById('slide-' + count.toString()).classList.remove('d-none');
