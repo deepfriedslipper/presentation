010d8afb20d8743b3e44a29d1f4f00192c4d9f9f
deepfriedslipper <108072844+deepfriedslipper@users.noreply.github.com>
2024-05-08 08:48:32 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.slide').forEach((slide) => {
-  slide.addEventListener('click', function () {
-    slide.classList.add('d-none');
-    index = parseInt(slide.id.slice(6)) + 1;
-    document.getElementById('slide-' + index.toString()).classList.remove('d-none');
-  });
-});
